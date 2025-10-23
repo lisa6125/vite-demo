80f34742fecddc8bcc61dcd35ac513bbe21f6f35
lisa6125 <aliceflowta@gmail.com>
2025-10-23 16:59:59 +0800

    tree-shaking use all module

--- src/Pages/Empty.tsx
+++ src/Pages/Empty.tsx
@@ -1,12 +1,25 @@
 import { Button, Stack, Typography } from "@mui/material";
+import { useState } from "react";
 import { useNavigate } from "react-router";
+import math from "../utils";
 
 export default function Empty() {
+  const [count, setCount] = useState(0);
   const navigate = useNavigate();
+  const handleCount = () => {
+    setCount(math.add(count));
+  };
   return (
     <Stack>
       <Typography>Empty page</Typography>
-      <Button onClick={() => navigate("calendar")}>go to calendar</Button>
+      <Button
+        onClick={() => {
+          navigate("calendar");
+          handleCount();
+        }}
+      >
+        go to calendar
+      </Button>
     </Stack>
   );
 }
