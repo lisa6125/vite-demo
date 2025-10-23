01cb98a836fffa19f0d2ff42bdd5a91543e245a6
lisa6125 <aliceflowta@gmail.com>
2025-10-23 16:59:20 +0800

    add utils

---
+++ src/utils.ts
@@ -0,0 +1,17 @@
+export const add = (val: number) => {
+  return val + 1;
+};
+
+export const sub = (val: number) => {
+  return val + 1;
+};
+
+export const mul = (val: number) => {
+  return val * 10;
+};
+
+export const divi = (val: number) => {
+  return val / 10;
+};
+
+export default { add, sub, mul, divi };
