e63adb1da2bb21569cbe27cb1bcad43b457d79b9
lisa6125 <aliceflowta@gmail.com>
2025-10-23 17:46:16 +0800

    Code Splitting - manualChunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig } from "vite";
+
+let chunkConfig = { core: [] };
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),visualizer({ filename: "dist/stats.html",open:true,gzipSize: true }) as any,],
-})
+  plugins: [
+    react(),
+    visualizer({
+      filename: "dist/stats.html",
+      open: true,
+      gzipSize: true,
+    }) as any,
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          const idIncludes = (keywords: string[] = []) =>
+            keywords.some((x) => id.includes(x));
+
+          if (idIncludes(["@mui", "ramda", ...(chunkConfig?.core ?? [])])) {
+            return "core";
+          }
+        },
+      },
+    },
+  },
+});
